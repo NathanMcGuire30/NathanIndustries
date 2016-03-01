be308bd812b31864c48390d970b9607a2b10774b
Nathan McGuire <nathan.e.mcguire@gmail.com>
2016-03-01 10:52:41 -0500

    updates

---
+++ src/main/java/com/nathan/nathanmod/items/TitaniumSword.java
@@ -0,0 +1,10 @@
+package com.nathan.nathanmod.items;
+
+import net.minecraft.item.ItemSword;
+
+public class TitaniumSword extends ItemSword {
+
+	public TitaniumSword(ToolMaterial p_i45356_1_) {
+		super(p_i45356_1_);
+	}
+}
